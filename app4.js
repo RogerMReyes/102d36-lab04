30b575b1e3ddc1e38e57092fab0483d25df0521c
Roger Reyes <rogerrmr14@gmail.com>
2022-02-24 15:06:25 -0800

    fixed text in app4

--- app4.js
+++ app4.js
@@ -1,8 +1,8 @@
 function steaksOnAGrill(){
     let baconGrillsImg = "https://baconmockup.com/640/360";
-    let answer = prompt("So how many Steaks should I lay on the grill?");
+    var answer = parseInt(prompt("So how many Steaks should I lay on the grill?"));
     if(answer > 0){
-        alert("Thats not enough that multiply that by 4!")
+        alert("Thats not enough that multiply that by 4!");
         for(let i=0; i<answer; i++){
             document.write((i+1).toString() + '<img class="Bacon" src="' + baconGrillsImg + '">');
         }
