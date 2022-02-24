f5e2a359d430f308bab544c3fb38feef7f0fceeb
Roger Reyes <rogerrmr14@gmail.com>
2022-02-24 13:27:41 -0800

    Added numbering to bacon image

--- app4.js
+++ app4.js
@@ -3,8 +3,8 @@ function steaksOnAGrill(){
     let answer = prompt("So how many Steaks should I lay on the grill?");
     if(answer > 0){
         alert("Thats not enough that multiply that by 4!")
-        for(i=0; i<answer; i++){
-            document.write('<img class="Bacon" src="' + baconGrillsImg + '">');
+        for(let i=0; i<answer; i++){
+            document.write((i+1).toString() + '<img class="Bacon" src="' + baconGrillsImg + '">');
         }
     } else {
         alert("Alright just 4 steaks on the grill for me then!")
